3ec698a219ea7dcd38ab6fa7d5aec83be1fb1942
Jcwells18 <jwells1@fhlbc.com>
2021-04-08 15:16:46 -0500

    updates to js 1

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -5,34 +5,37 @@ console.log(tableData)
 
 var tbody = d3.select("tbody");
 
-//show entire dataset
+//table data for entire set
 
 tableData.forEach((ufoData) =>{
     console.log(ufoData);
+    // append table row for each UFO data
     var row = tbody.append('tr');
-
+    //console log each value 
     Object.entries(ufoData).forEach(([key,value]) => {
         console.log(key,value);
+        //append value to each row
         var cell = row.append('td');
         cell.text(value);
 
     });
 });
-
+//create button
 var button = d3.select("#filter-btn");
 
 button.on("click", function(event){
     d3.event.preventDefault();
     tbody.html("");
-
+//find input
 var inputElement = d3.select("#datetime");
-
+//value for input
 var inputValue = inputElement.property("value");
 
+//filter data with datetime === to inputValue
 var filteredData = tableData.filter(tableData => tableData.datetime === inputValue);
-filteredData.forEach(function(dataDate){
+filteredData.forEach(function(selected){
     var row=tbody.append("tr");
-    Object.entries(dataDate).forEach(function([key,value]){
+    Object.entries(selected).forEach(function([key,value]){
         var cell=tbody.append("td");
         cell.text(value);
 
